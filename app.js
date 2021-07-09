76a3e727775974935815de7d200a84996054f875
Mikel8Lee <connerrobert927@gmail.com>
2021-07-09 18:53:09 +0530

    Added audio files

---
+++ app.js
@@ -0,0 +1,32 @@
+//Importing express and all other modules on top
+const express = require("express");
+const app = express();
+const path = require("path");
+const ejsMate = require("ejs-mate");
+const methodOverride = require("method-override");
+
+//Setting up the view engine and it's directory
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+//All Express middleware/ Static files
+app.use(express.urlencoded({ extended: true })); //req.body is parsed as a form
+app.use(methodOverride("_method")); //Setting the query for method-override
+app.use(express.static(path.join(__dirname, "public"))); //It will serve our static files
+
+//Default Index Route
+//We don't need to add backslash when rendering view pages
+app.get("/", (req, res) => {
+  res.send("Welcome to Visibility");
+});
+
+//Homepage Route
+app.get("/home", (req, res) => {
+  res.render("home/home.ejs");
+});
+
+//Starting up server
+app.listen(3000, () => {
+  console.log("You are listening at PORT: 3000");
+});
